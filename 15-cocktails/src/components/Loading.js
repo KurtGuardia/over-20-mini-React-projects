3fd3320ffd793fab2f6a00e679777ca54c4fe600
Kurt Guardia <kurtguardia@gmail.com>
2020-10-07 13:42:25 +0200

    Fifteenth project Cocktails done

---
+++ 15-cocktails/src/components/Loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div className="loader">hjk</div>;
+};
+
+export default Loading;
---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex quibusdam
+        debitis vitae facilis numquam voluptatibus aliquid impedit itaque
+        eligendi porro omnis fugiat repellendus placeat laboriosam, optio esse
+        molestiae minus adipisci laborum ea quam aliquam, consectetur eos
+        ratione. Doloremque, accusantium quae.
+      </p>
+    </section>
+  );
+};
+
+export default About;
